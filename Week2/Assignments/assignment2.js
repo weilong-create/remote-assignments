07c9dfcd13cf7760b5e562b1f16d806f91c55e5e
weilong-create <4603143264f@gmail.com>
2022-09-20 10:23:42 +0800

    Create assignment2.js

---
+++ Week2/Assignments/assignment2.js
@@ -0,0 +1,17 @@
+function delayedResult(n1, n2, delaytime, callback){
+    // your code here
+    setTimeout(function(){
+        callback(n1+n2);
+    }, delaytime)
+
+}
+
+delayedResult(4, 5, 3000, function(result)
+{
+  console.log(result);
+})
+
+delayedResult(-5, 10, 2000, function(result)
+{
+  console.log(result);
+})
